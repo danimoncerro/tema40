b5f38c999ea26844c8016b29aca151f80a82fe48
danimoncerro <dani_moncerro@yahoo.com>
2017-04-21 22:09:39 +0300

    new routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/classes', 'ClassController@index');
+Route::get('/students', 'StudentController@index');
+
+
